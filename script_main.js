c320d902946e48353d0781d22edf1adf417ac2ca
SotsuHub <124271898+SotsuHub@users.noreply.github.com>
2023-03-27 00:55:17 +0900

    Add files via upload

---
+++ script_main.js
@@ -0,0 +1,35 @@
+"use strict";
+// Please don't delete the 'use strict' line above
+
+let notPushButton = document.getElementById("notPushButton");
+let unicorn = document.getElementById("unicorn");
+
+// gifを配列に入れる
+const url = "https://github.com/SotsuHub/SotsuHub.github.io/blob/main/pictures/";
+const gifCount = 4;
+const pictures = [];
+for (let i = 0; i <= gifCount; i++) {
+    pictures[i] = `${url}unicorn_${i}.gif`;
+}
+
+// ボタンが押された処理
+notPushButton.addEventListener("click", notPushButtonClick);
+
+async function notPushButtonClick() {
+    for (const element of pictures) {
+        unicorn.src = element;
+        await wait(2);
+    }
+    unicorn.src = url + "unicorn_none.png";
+    await wait(5);
+    unicorn.src = url + "unicorn_half.gif";
+    await wait(3);
+    unicorn.src = url + "unicorn_0.gif";
+}
+
+// 待機処理
+const wait = (sec) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, sec * 1000);
+    });
+};

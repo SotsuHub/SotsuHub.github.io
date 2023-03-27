4741578194224f42e67a34eed7cc0d3f676f0d66
SotsuHub <124271898+SotsuHub@users.noreply.github.com>
2023-03-27 13:46:09 +0900

    Update script_main.js

--- script_main.js
+++ script_main.js
@@ -1,8 +1,11 @@
 "use strict";
 // Please don't delete the 'use strict' line above
 
+// 初期設定
 let notPushButton = document.getElementById("notPushButton");
 let unicorn = document.getElementById("unicorn");
+let count = 1;
+let runFlag = 0;
 
 // gifを配列に入れる
 const url = "pictures/";
@@ -16,15 +19,34 @@ for (let i = 0; i <= gifCount; i++) {
 notPushButton.addEventListener("click", notPushButtonClick);
 
 async function notPushButtonClick() {
-    for (const element of pictures) {
-        unicorn.src = element;
-        await wait(2);
+    console.log("count:" + count);
+    if (runFlag === 0) {
+        if (count === 1 || count === 2) {
+            runFlag = 1;
+            unicorn.src = url + "unicorn_0.gif";
+            await wait(4);
+            unicorn.src = url + "unicorn_1.gif";
+            count++;
+            runFlag = 0;
+        } else if (count === 3) {
+            runFlag = 1;
+            for (const element of pictures) {
+                unicorn.src = element;
+                await wait(2.5);
+            }
+            unicorn.src = url + "unicorn_none.png";
+            await wait(7);
+            unicorn.src = url + "unicorn_half.gif";
+            await wait(7);
+            unicorn.src = url + "unicorn_0.gif";
+            await wait(7);
+            unicorn.src = url + "unicorn_1.gif";
+            count = 1;
+            runFlag = 0;
+        } else {
+            count = 1;
+        }
     }
-    unicorn.src = url + "unicorn_none.png";
-    await wait(6);
-    unicorn.src = url + "unicorn_half.gif";
-    await wait(6);
-    unicorn.src = url + "unicorn_0.gif";
 }
 
 // 待機処理
